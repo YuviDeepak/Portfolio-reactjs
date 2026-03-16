6d88afb7ec0beed8fc2cd60192139848df44eba3
YuviDeepak <yuvideepak190@gmail.com>
2026-03-16 16:20:01 +0530

    portfolio modified

--- src/components/Links/Lnks.jsx
+++ src/components/Links/Lnks.jsx
@@ -8,6 +8,7 @@ export const Lnks = () => {
                     <a href="https://wa.me/6381243283" target='_blank' className='aaa'><div className="LIICONS"><i className="fa-brands fa-whatsapp"></i></div></a>
                     <a href="https://www.linkedin.com/in/deepak-b-4a6943251/" className='aaa'><div className="LIICONS"><i className="fa-brands fa-linkedin-in"></i></div></a>
                     <a href="https://github.com/YuviDeepak" className='aaa'><div className="LIICONS"><i class="fa-brands fa-github"></i></div></a>
+                    
             </div>
         </>
     )
