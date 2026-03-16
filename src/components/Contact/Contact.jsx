8b0db3dcb8ef062525a3fd1fce1095ccd90efc54
YuviDeepak <yuvideepak190@gmail.com>
2026-03-16 14:16:17 +0530

    portfolio modified

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -23,6 +23,7 @@ export const Contact = () => {
                         <input type="text" placeholder='Phone Number' id='phno'/>
                         <input type="text" placeholder='Subject' id='subject'/>
                         <textarea placeholder='Message' id='msg'></textarea>
+                        <button>Send <i class="fa-solid fa-paper-plane"></i></button>
                     </div>
                 </div>
             </div>
