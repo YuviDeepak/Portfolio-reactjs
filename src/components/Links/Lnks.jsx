b18d92224e8bb6d752714918e613301576850adb
YuviDeepak <yuvideepak190@gmail.com>
2026-03-16 16:24:02 +0530

    portfolio modified

--- src/components/Links/Lnks.jsx
+++ src/components/Links/Lnks.jsx
@@ -6,8 +6,8 @@ export const Lnks = () => {
             <div className="LLiinnkkss">
                     <a href="mailto:yuvideepak190@gmail.com" target='_blank' className='aaa'><div className="LIICONS"><i className="fa-regular fa-envelope" ></i></div></a>
                     <a href="https://wa.me/6381243283" target='_blank' className='aaa'><div className="LIICONS"><i className="fa-brands fa-whatsapp"></i></div></a>
-                    <a href="https://www.linkedin.com/in/deepak-b-4a6943251/" className='aaa'><div className="LIICONS"><i className="fa-brands fa-linkedin-in"></i></div></a>
-                    <a href="https://github.com/YuviDeepak" className='aaa'><div className="LIICONS"><i className="fa-brands fa-github"></i></div></a>
+                    <a href="https://www.linkedin.com/in/deepak-b-4a6943251/" target='_blank' className='aaa'><div className="LIICONS"><i className="fa-brands fa-linkedin-in"></i></div></a>
+                    <a href="https://github.com/YuviDeepak" target='_blank' className='aaa'><div className="LIICONS"><i className="fa-brands fa-github"></i></div></a>
                     
             </div>
         </>
