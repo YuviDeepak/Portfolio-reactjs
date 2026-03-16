f1031341cc6ee2feaf0495e8968d6069ae8905da
YuviDeepak <yuvideepak190@gmail.com>
2026-03-16 16:17:51 +0530

    portfolio modified

--- src/components/Links/Lnks.jsx
+++ src/components/Links/Lnks.jsx
@@ -4,10 +4,10 @@ export const Lnks = () => {
     return (
         <>
             <div className="LLiinnkkss">
-                    <a href="" className='aaa'><div className="LIICONS"><i class="fa-regular fa-envelope" ></i></div></a>
-                    <a href="" className='aaa'><div className="LIICONS"><i className="fa-brands fa-whatsapp"></i></div></a>
-                    <a href="" className='aaa'><div className="LIICONS"><i className="fa-brands fa-linkedin-in"></i></div></a>
-                    <a href="" className='aaa'><div className="LIICONS"><i class="fa-brands fa-github"></i></div></a>
+                    <a href="mailto:yuvideepak190@gmail.com" target='_blank' className='aaa'><div className="LIICONS"><i class="fa-regular fa-envelope" ></i></div></a>
+                    <a href="https://wa.me/6381243283" target='_blank' className='aaa'><div className="LIICONS"><i className="fa-brands fa-whatsapp"></i></div></a>
+                    <a href="https://www.linkedin.com/in/deepak-b-4a6943251/" className='aaa'><div className="LIICONS"><i className="fa-brands fa-linkedin-in"></i></div></a>
+                    <a href="https://github.com/YuviDeepak" className='aaa'><div className="LIICONS"><i class="fa-brands fa-github"></i></div></a>
             </div>
         </>
     )
